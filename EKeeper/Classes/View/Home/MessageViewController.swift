2a9214c8870b00db4fc6c4f7f45a223560fcdc5b
kuyu310 <28190385@qq.com>
2017-03-31 10:42:24 +0800

    tijao

---
+++ EKeeper/Classes/View/Home/MessageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewCV.swift
+//  EKeeper
+//
+//  Created by limeng on 2017/3/30.
+//  Copyright © 2017年 limeng. All rights reserved.
+//
+
+import Foundation
+
+class MessageViewController: KeeperBaseViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.randomColor()
+        
+        let bt = UIButton(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
+        bt.titleLabel?.text = "点我"
+        bt.backgroundColor = UIColor.yellow
+        //“”
+    }
+}
